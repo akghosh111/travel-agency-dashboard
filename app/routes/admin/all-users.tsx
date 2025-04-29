53c3f28b44a743bfbb07a5139eddb2e6503caccb
akghosh111 <ghoshkiran2017@gmail.com>
2025-04-29 16:33:15 +0530

    adding mobile & desktop sidebar

--- app/routes/admin/all-users.tsx
+++ app/routes/admin/all-users.tsx
@@ -1,8 +1,16 @@
+import { Header } from "components"
+
 const AllUsers = () => {
   return (
-    <div>
-      Users Table
-    </div>
+    <main className="dashboard wrapper">
+
+      <Header
+              title = "Trips page"
+              description="Check out our current users in real time"
+      />
+
+      All Users Page Contents
+    </main>
   )
 }
 
--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -1,8 +1,18 @@
+import { Header } from "components"
+
 const dashboard = () => {
+
+  const user = { name: "David"}
   return (
-    <div>
-      Dashboard Page
-    </div>
+    <main className="dashboard wrapper">
+      <Header
+        title = {`Welcome ${user?.name ?? "Guest"} 👋`}
+        description="Track activity, trends and popular destinations in real time"
+      />
+
+      Dashboard Page Contents
+
+    </main>
   )
 }
 
