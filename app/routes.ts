f607ba449d9351561e2043793dd4c82849266a45
akghosh111 <ghoshkiran2017@gmail.com>
2025-04-30 22:23:28 +0530

    auth logic

--- app/routes.ts
+++ app/routes.ts
@@ -2,6 +2,7 @@ import { type RouteConfig, layout, route } from "@react-router/dev/routes";
 import path from "path";
 
 export default [
+    route("sign-in", "routes/root/sign-in.tsx"),
     layout("routes/admin/admin-layout.tsx",[
 
         route("dashboard", "routes/admin/dashboard.tsx"),
